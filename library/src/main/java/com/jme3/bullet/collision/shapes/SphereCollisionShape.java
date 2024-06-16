85500acd39fbcc807e9b36cbd773d0425c415e86
stephengold <sgold@sonic.net>
2024-06-15 18:46:29 -0700

    library:  add the SphereCollisionShape class

---
+++ library/src/main/java/com/jme3/bullet/collision/shapes/SphereCollisionShape.java
@@ -0,0 +1,111 @@
+/*
+ * Copyright (c) 2009-2018 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jme3.bullet.collision.shapes;
+
+import com.jme3.bullet.PhysicsSpace;
+import java.lang.foreign.MemorySession;
+import java.util.logging.Logger;
+import jme3utilities.Validate;
+import jolt.Jolt;
+import jolt.physics.collision.shape.Shape;
+import jolt.physics.collision.shape.SphereShapeSettings;
+
+/**
+ * A spherical collision shape.
+ *
+ * @author normenhansen
+ */
+public class SphereCollisionShape extends CollisionShape {
+    // *************************************************************************
+    // loggers
+
+    /**
+     * message logger for this class
+     */
+    final public static Logger logger2
+            = Logger.getLogger(SphereCollisionShape.class.getName());
+    // *************************************************************************
+    // fields
+
+    /**
+     * copy of the radius (&ge;0)
+     */
+    private float radius;
+    // *************************************************************************
+    // constructors
+
+    /**
+     * No-argument constructor needed by SavableClassUtil.
+     */
+    protected SphereCollisionShape() {
+    }
+
+    /**
+     * Instantiate a sphere shape with the specified radius.
+     *
+     * @param radius the desired unscaled radius (in shape units, &ge;0)
+     */
+    public SphereCollisionShape(float radius) {
+        Validate.nonNegative(radius, "radius");
+
+        this.radius = radius;
+        createShape();
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the radius of the sphere.
+     *
+     * @return the unscaled radius (in shape units, &ge;0)
+     */
+    public float getRadius() {
+        assert radius >= 0f : radius;
+        return radius;
+    }
+    // *************************************************************************
+    // Java private methods
+
+    /**
+     * Instantiate the configured shape.
+     */
+    private void createShape() {
+        assert radius >= 0f : radius;
+
+        MemorySession arena = PhysicsSpace.getArena();
+        SphereShapeSettings bss = SphereShapeSettings.of(radius);
+        Shape shape = Jolt.use(bss, settings -> {
+            return settings.create(arena);
+        }).orThrow();
+        setNativeObject(shape);
+    }
+}
