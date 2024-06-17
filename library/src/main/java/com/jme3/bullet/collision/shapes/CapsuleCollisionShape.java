c10a7ad5559932e77f600f67d917c456b77e8a4f
stephengold <sgold@sonic.net>
2024-06-17 15:58:53 -0700

    library: add the CapsuleCollisionShape class

---
+++ library/src/main/java/com/jme3/bullet/collision/shapes/CapsuleCollisionShape.java
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2009-2018 jMonkeyEngine
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are
+ * met:
+ *
+ * * Redistributions of source code must retain the above copyright
+ *   notice, this list of conditions and the following disclaimer.
+ *
+ * * Redistributions in binary form must reproduce the above copyright
+ *   notice, this list of conditions and the following disclaimer in the
+ *   documentation and/or other materials provided with the distribution.
+ *
+ * * Neither the name of 'jMonkeyEngine' nor the names of its contributors
+ *   may be used to endorse or promote products derived from this software
+ *   without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+ * TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+ * PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package com.jme3.bullet.collision.shapes;
+
+import com.jme3.bullet.PhysicsSpace;
+import java.lang.foreign.MemorySession;
+import java.util.logging.Logger;
+import jme3utilities.Validate;
+import jolt.Jolt;
+import jolt.physics.collision.shape.CapsuleShapeSettings;
+import jolt.physics.collision.shape.Shape;
+
+/**
+ * A capsule collision shape based on jolt-java's {@code CapsuleShape}.
+ *
+ * @author normenhansen
+ */
+public class CapsuleCollisionShape extends CollisionShape {
+    // *************************************************************************
+    // loggers
+
+    /**
+     * message logger for this class
+     */
+    final public static Logger logger2
+            = Logger.getLogger(CapsuleCollisionShape.class.getName());
+    // *************************************************************************
+    // fields
+
+    /**
+     * copy of the height of the cylindrical portion (&ge;0)
+     */
+    private float height;
+    /**
+     * copy of the radius (&ge;0)
+     */
+    private float radius;
+    // *************************************************************************
+    // constructors
+
+    /**
+     * No-argument constructor needed by SavableClassUtil.
+     */
+    protected CapsuleCollisionShape() {
+    }
+
+    /**
+     * Instantiate a Y-axis capsule shape with the specified radius and height.
+     *
+     * @param radius the desired radius (&ge;0)
+     * @param height the desired height of the cylindrical portion (&ge;0)
+     */
+    public CapsuleCollisionShape(float radius, float height) {
+        Validate.nonNegative(radius, "radius");
+        Validate.nonNegative(height, "height");
+
+        this.radius = radius;
+        this.height = height;
+        createShape();
+    }
+    // *************************************************************************
+    // new methods exposed
+
+    /**
+     * Return the main (height) axis of the capsule.
+     *
+     * @return 1
+     */
+    public int getAxis() {
+        return PhysicsSpace.AXIS_Y;
+    }
+
+    /**
+     * Return the height of the cylindrical portion.
+     *
+     * @return the height (&ge;0)
+     */
+    public float getHeight() {
+        assert height >= 0f : height;
+        return height;
+    }
+
+    /**
+     * Return the radius of the capsule.
+     *
+     * @return the radius (&ge;0)
+     */
+    public float getRadius() {
+        assert radius >= 0f : radius;
+        return radius;
+    }
+    // *************************************************************************
+    // Java private methods
+
+    /**
+     * Instantiate the configured shape.
+     */
+    private void createShape() {
+        assert radius >= 0f : radius;
+        assert height >= 0f : height;
+
+        float halfHeight = height / 2f;
+        CapsuleShapeSettings css = CapsuleShapeSettings.of(halfHeight, radius);
+        MemorySession arena = PhysicsSpace.getArena();
+        Shape shape = Jolt.use(css, settings -> {
+            return settings.create(arena);
+        }).orThrow();
+        setNativeObject(shape);
+    }
+}
