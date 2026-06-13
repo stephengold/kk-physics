214cc3e576038b8ac57cb093dd06c153471040f7
stephengold <sgold@sonic.net>
2026-06-13 16:57:58 -0700

    update the Jolt-JNI library to v5.1.0

--- library/src/main/java/com/jme3/bullet/util/NativeLibrary.java
+++ library/src/main/java/com/jme3/bullet/util/NativeLibrary.java
@@ -65,7 +65,7 @@ final public class NativeLibrary {
     /**
      * expected version string of the native library
      */
-    final public static String expectedVersion = "5.0.0";
+    final public static String expectedVersion = "5.1.0";
     // *************************************************************************
     // constructors
 
